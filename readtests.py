89fd3f014733082021cbbfca0a0dd342b1870929
Juan J <shurkun@gmail.com>
2022-07-09 11:38:03 +0200

    minor changes

--- readtests.py
+++ readtests.py
@@ -1,12 +1,10 @@
-from tkinter.messagebox import QUESTION
-import yaml  # PyYAML
+import yaml # PyYAML
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-
 class RT:
     def __init__(self):
-        self.title = None
+        self.topic = None
         self.questions = {0: {'TITLE': 'You have 15 seconds to answer',
                               'answers': ['This show the options in the test',
                                           'Only one is correct, search the matching button',
@@ -14,13 +12,13 @@ class RT:
                                           'Have fun!'],
                               'correct': 0}}
         self.read_questions()
-
+    
     # Abre el archivo y carga los datos
     def open_test(self, route):
         with open(route, encoding="utf8") as file:
                 test = yaml.safe_load(file)
-
-        self.title = test['TOPIC']
+                
+        self.topic = test['TOPIC']
         self.questions.update(test['TEST'])
 
     # Recibe el nombre de archivo y comprueba que se puede abrir
@@ -28,10 +26,10 @@ class RT:
         root = Tk()
         filetypes = (('yaml files', '*.yaml'), ('yaml files', '*.yml'))
         root.withdraw()
-
-        test = askopenfilename(initialdir="./tests/", filetypes=filetypes)
-
-        try:
+        
+        test = askopenfilename(initialdir="./tests/",filetypes=filetypes)
+            
+        try:    
             self.open_test(route=test)
 
         except FileNotFoundError:
