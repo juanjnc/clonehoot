83c005e710c2fe75a42795b04f91dc545e277f33
juanjnc <shurkun@gmail.com>
2023-03-20 19:56:06 +0100

    now shuffle the test

--- readtests.py
+++ readtests.py
@@ -1,12 +1,12 @@
 import yaml  # PyYAML
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-
+from random import shuffle
 
 class RT:
     def __init__(self):
         self.topic = None
-        self.questions = {0: {'TITLE': 'You have 15 seconds to answer',
+        self.questions = {0: {'TITLE': f'You have 15 seconds to answer',
                               'answers': ['This show the options in the test',
                                           'Only one is correct, search the matching button',
                                           'Press now any button to continue',
@@ -18,9 +18,17 @@ class RT:
     def open_test(self, route):
         with open(route, encoding="utf8") as file:
             test = yaml.safe_load(file)
-
+            
+        # Mezcla las preguntas
+        order = (list(test['TEST'].values()))
+        shuffle(order)
+        # Actualiza el nuevo orden con la numeración correcta
+        battery = dict()
+        for item in order: battery.update({(order.index(item)+1): item})
+        
         self.topic = test['TOPIC']
-        self.questions.update(test['TEST'])
+        self.questions.update(battery)
+
 
     # Recibe el nombre de archivo y comprueba que se puede abrir
     def read_questions(self):
